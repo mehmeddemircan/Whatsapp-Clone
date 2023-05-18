8053d9b8d806816a497f4034c803d7425beff0d3
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-18 20:57:38 +0300

    created private route

---
+++ frontend/src/routes/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+// Custom PrivateRoute component
+const PrivateRoute = ({ children }) => {
+  const login = useSelector((state) => state.user.login);
+
+
+  // Render loading state if authentication status is still being determined
+
+  if (login.authenticate) {
+    // Render the protected page if user is authenticated
+    return children;
+  } else {
+    // Redirect to login page if user is not authenticated
+    return <Navigate to="/login" replace />;
+  }
+};
+export default PrivateRoute;
