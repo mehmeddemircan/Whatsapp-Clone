d8798944a311f8d96c3ef665654818bee41b79c0
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-18 20:57:31 +0300

    added store to index .js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // antd css
 import 'antd/dist/reset.css';
+// redux 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
