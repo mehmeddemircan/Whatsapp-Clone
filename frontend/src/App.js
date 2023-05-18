7b82777f5278291ac68a1630fe29de6118f32b0f
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-18 20:56:20 +0300

    added private route to prevent to route chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 
 import { BrowserRouter as Router , Routes,Route } from 'react-router-dom'
-import HomePage from './pages/HomePage';
+
+import AuthPage from './pages/AuthPage';
+import ChatPage from './pages/ChatPage';
+import ChatDetailsPage from './pages/ChatDetailsPage';
+import PrivateRoute from './routes/PrivateRoute';
 function App() {
   return (
  <Router>
   <Routes>
-    <Route index path='/' element={<HomePage />} />
+    <Route index path='/login' element={<AuthPage />} />
+
+    {/* Private Route */}
+    <Route path='/chat' element={<PrivateRoute>
+      <ChatPage />
+      </PrivateRoute>} />
+    <Route path='/chat/:id' element={<ChatDetailsPage />} />
   </Routes>
  </Router>
   );
