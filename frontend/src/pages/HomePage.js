edfad0a2949830e9c97e33390776f6aa2ba0b2e9
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-18 18:46:25 +0300

    added meta title to home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,16 @@
+import { Button, Space } from 'antd'
 import React from 'react'
+import MetaTitle from '../meta/MetaTitle'
 
 const HomePage = () => {
   return (
     <div>
+        <MetaTitle title="Whatsapp Web" name="Whatsappweb" content="whatsappweb" />
             <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-
-    </div>
+    
+  </div>
   )
 }
 
